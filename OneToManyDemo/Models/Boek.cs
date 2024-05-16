d38d6fc4eba942c9542c71c6df01a8171371f07f
Kenan Kurda <intecbrussel24@gmail.com>
2024-05-16 15:27:01 +0200

    added fk in annotations

--- OneToManyDemo/Models/Boek.cs
+++ OneToManyDemo/Models/Boek.cs
@@ -1,9 +1,13 @@
-﻿namespace OneToManyDemo.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OneToManyDemo.Models
 {
     public class Boek
     {
         public int BoekId { get; set; }
         public string Titel { get; set; }
+
+        [ForeignKey("Auteur")]
         public int AuteurId { get; set; }
         public Auteur Auteur { get; set; }
     }
