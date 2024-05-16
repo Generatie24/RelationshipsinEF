13b7b7222cd9c811c956dc9445d5ab0a1a8a1270
Kenan Kurda <intecbrussel24@gmail.com>
2024-05-16 15:05:48 +0200

    Add project files.

---
+++ OneToManyDemo/Controllers/BoekenController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OneToManyDemo.Data;
+using OneToManyDemo.Models.ViewModels;
+
+namespace OneToManyDemo.Controllers
+{
+    public class BoekenController : Controller
+    {
+        readonly BoekenDbContext _context;
+        public BoekenController(BoekenDbContext dbContext)
+        {
+                _context = dbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var boekAuteurViewModel = _context.Boeks
+                .Include(b => b.Auteur)
+                .Select(b => new BoekAuteurViewModel
+                {
+                    BoekId = b.BoekId,
+                    Titel = b.Titel,
+                    AuteurNaam = b.Auteur.Naam
+                });
+
+            return View(boekAuteurViewModel);
+        }
+    }
+}
---
+++ OneToManyDemo/Data/BoekenDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OneToManyDemo.Models;
+using OneToManyDemo.Models.ViewModels;
+
+namespace OneToManyDemo.Data
+{
+    public class BoekenDbContext : DbContext
+    {
+        public BoekenDbContext(DbContextOptions<BoekenDbContext> options) : base(options) { }
+        public DbSet<Auteur> Auteurs { get; set; }
+        public DbSet<Boek> Boeks { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // configure Auteur entity
+            modelBuilder.Entity<Auteur>()
+                .HasKey(a => a.AuteurId);
+            modelBuilder.Entity<Auteur>()
+                .HasMany(a => a.Boeken)
+                .WithOne(b => b.Auteur)
+                .HasForeignKey(b => b.AuteurId)
+                .OnDelete(DeleteBehavior.NoAction);
+            
+
+            SeedData.AddRecords(modelBuilder);
+        }
+    }
+}
